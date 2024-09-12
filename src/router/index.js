d5cd73b8987edb9b0a3153a219ad70e07658f4c2
sam <samorton46@gmail.com>
2024-09-12 21:10:05 +0100

    attempitng to make it work on github pages v12

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ProjectsView from '../views/ProjectsView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'; // Switch to hash mode
+import HomeView from '../views/HomeView.vue';
+import ProjectsView from '../views/ProjectsView.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/projects',
-    name: 'projects',
-    component: ProjectsView
-  }
-]
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/projects', name: 'projects', component: ProjectsView }
+];
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
-})
+});
 
-export default router
+export default router;
