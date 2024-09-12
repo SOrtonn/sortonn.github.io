f309c9dee5a6ca17a6083d01a9112c0ed9888570
sam <samorton46@gmail.com>
2024-09-12 21:03:52 +0100

    attempitng to make it work on github pages v10

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-try {
-  const app = createApp(App)
-  app.use(router)
-  app.mount('#app')
-  console.log('Vue app mounted successfully')
-} catch (error) {
-  console.error('Error initializing Vue app:', error)
-  document.body.innerHTML += `<p>Error initializing Vue app: ${error.message}</p>`
-}
+createApp(App).use(router).mount('#app');
